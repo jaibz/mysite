3e8b01a3a4136deb4a442fbc6de1f82a8d31a93a
jaibz <jaibhaarath47@gmail.com>
2024-08-06 19:34:41 +0530

    site changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Footer = () => (
+  <footer className="footer">
+    <nav className="social-links">
+      <a href="https://www.truevote.in" target="_blank" rel="noopener noreferrer">TrueVote <span className="arrow">&#x2197;</span></a>
+      <a href="https://www.linkedin.com/in/jai-bhaarath" target="_blank" rel="noopener noreferrer">LinkedIn <span className="arrow">&#x2197;</span></a>
+      <a href="https://www.instagram.com/jaibhaarath" target="_blank" rel="noopener noreferrer">Instagram <span className="arrow">&#x2197;</span></a>
+      <a href="https://www.twitter.com/jaibz" target="_blank" rel="noopener noreferrer">Twitter <span className="arrow">&#x2197;</span></a>
+    </nav>
+  </footer>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <main className="content">
+        <h1 id="my-name">Jai Bhaarath</h1>
+        <a id="my-mail" href="mailto:jb@jaibhaarath.org">jb@jaibhaarath.org</a>
+      </main>
+      <Footer />
     </div>
   );
 }
