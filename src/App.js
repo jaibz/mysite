c79bf53e716b44685f90c5a0cc30e57d2592de46
jaibz <jaibhaarath47@gmail.com>
2024-12-30 11:57:44 +0530

    removed insta

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ const Footer = () => (
   <footer className="footer">
     <nav className="social-links">
       <a href="https://www.truevote.in" target="_blank" rel="noopener noreferrer">TrueVote <span className="arrow">&#x2197;</span></a>
-      <a href="https://www.linkedin.com/in/jai-bhaarath" target="_blank" rel="noopener noreferrer">LinkedIn <span className="arrow">&#x2197;</span></a>
-      <a href="https://www.instagram.com/jaibz_" target="_blank" rel="noopener noreferrer">Instagram <span className="arrow">&#x2197;</span></a>
       <a href="https://x.com/intent/follow?screen_name=jaibz" target="_blank" rel="noopener noreferrer">Twitter <span className="arrow">&#x2197;</span></a>
+      <a href="https://www.instagram.com/jaibz_" target="_blank" rel="noopener noreferrer">Instagram <span className="arrow">&#x2197;</span></a>
     </nav>
   </footer>
 );
